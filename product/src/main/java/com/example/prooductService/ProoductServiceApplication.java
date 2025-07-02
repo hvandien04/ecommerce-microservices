97712bac1cbdd3bb714c2ddbbe924fbf87eb72a8
Hoàng Văn Diện <hoangvandien051004@gmail.com>
2025-07-02 11:10:31 +0700

    update

---
+++ product/src/main/java/com/example/prooductService/ProoductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.prooductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProoductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProoductServiceApplication.class, args);
+	}
+
+}
